b11c58d53c70c40b4af8cc53b396f95699222b1a
bryangregory-hub <s10207917@connect.np.edu.sg>
2020-11-27 23:33:17 +0800

    final

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ var mybutton = document.getElementById("myBtn");
 
 // When the user scrolls down 20px from the top of the document, show the button
 window.onscroll = function() {scrollFunction()};
-
+// public var
 function scrollFunction() {
   if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
     mybutton.style.display = "block";
